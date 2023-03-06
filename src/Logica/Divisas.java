92775479e1eba5f2b83a4eaac1fe2c27e0a55497
Marl <winering@gmail.com>
2023-03-06 12:13:29 -0300

    Obtimizando codigo

--- src/Logica/Divisas.java
+++ src/Logica/Divisas.java
@@ -17,79 +17,61 @@ public class Divisas {
             resultado = valor/193.19;
             // Mustra solo los primeros decimales de un double
             resultado = (double) Math.round(resultado * 100d)/100;
-            return resultado;
             
         }
         else if(tipo.equalsIgnoreCase("Peso ARS - Euro")){
         
             resultado = valor/206.53;
-            resultado = (double) Math.round(resultado * 100d)/100;
-            return resultado;
-          
+            resultado = (double) Math.round(resultado * 100d)/100;        
         }
         else if(tipo.equalsIgnoreCase("Peso ARS - Libra Esterlina")){
         
             resultado = valor/232.62;
-            resultado = (double) Math.round(resultado * 100d)/100;
-            return resultado;
-            
+            resultado = (double) Math.round(resultado * 100d)/100;         
         }   
         else if(tipo.equalsIgnoreCase("Peso ARS - Yen")){
         
             resultado = valor/144;
-            resultado = (double) Math.round(resultado * 100d)/100;
-            return resultado;
-         
+            resultado = (double) Math.round(resultado * 100d)/100;      
         }   
         else if(tipo.equalsIgnoreCase("Peso ARS - Won sul")){
         
             resultado = valor/6.74;
-            resultado = (double) Math.round(resultado * 100d)/100;
-            return resultado;
-            
+            resultado = (double) Math.round(resultado * 100d)/100;     
         } 
         else if(tipo.equalsIgnoreCase("Dolar - Peso ARS")) {
         
             resultado = valor*193.19;
-            resultado = (double) Math.round(resultado * 100d)/100;
-            return resultado;
-            
+            resultado = (double) Math.round(resultado * 100d)/100;    
         }
         else if(tipo.equalsIgnoreCase("Euro - Peso ARS")){
         
             resultado = valor*206.53;
-            resultado = (double) Math.round(resultado * 100d)/100;
-            return resultado;
-            
+            resultado = (double) Math.round(resultado * 100d)/100;         
         }
         else if(tipo.equalsIgnoreCase("Libra Esterlina - Peso ARS")){
         
             resultado = valor*232.62;
-            resultado = (double) Math.round(resultado * 100d)/100;
-            return resultado;
-            
+            resultado = (double) Math.round(resultado * 100d)/100;   
         }   
         else if(tipo.equalsIgnoreCase("Yen - Peso ARS")){
         
             resultado = valor*144;
-            resultado = (double) Math.round(resultado * 100d)/100;
-            return resultado;
-            
+            resultado = (double) Math.round(resultado * 100d)/100;        
         }   
         else if(tipo.equalsIgnoreCase("Won sul - Peso ARS")){
         
             resultado = valor*6.74;
-            resultado = (double) Math.round(resultado * 100d)/100;
-            return resultado;
-            
+            resultado = (double) Math.round(resultado * 100d)/100;    
         } 
         else {
             JOptionPane.showMessageDialog(null,
                 "Error no se ha podido realizar la operación",
                 "Error",
                 JOptionPane.ERROR_MESSAGE);
-            return 0;
-        }
+            resultado = 0;
+        }    
+        return resultado;
     }  
    
 }
--- src/Logica/Temperatura.java
+++ src/Logica/Temperatura.java
@@ -15,20 +15,19 @@ public class Temperatura {
             resultado = (valor*1.8) + 32;
             // Mustra solo los primeros decimales de un double
             resultado = (double) Math.round(resultado * 100d)/100;
-            return resultado;
         }
         else if (tipo.equalsIgnoreCase("Fahrenheit a Celcius")) {
             
             resultado = (valor - 32) / 1.8;
             resultado = (double) Math.round(resultado * 100d)/100;
-            return resultado;
         }
         else {
              JOptionPane.showMessageDialog(null,
                 "Error no se ha podido realizar la operación",
                 "Error",
                 JOptionPane.ERROR_MESSAGE);
-             return 0;
+             resultado = 0;
         }
+    return resultado;
     }  
 }
